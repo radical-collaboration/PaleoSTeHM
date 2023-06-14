79ec50809e59076c19e679ceab74c9d5240023d6
Yucheng Lin <42965663+yc-lin-geo@users.noreply.github.com>
2023-06-14 11:10:34 -0400

    Add files via upload

--- PSTHM.py
+++ PSTHM.py
@@ -269,6 +269,152 @@ class GPRegression_V(GPModel):
         return lambda xnew: sample_next(xnew, outside_vars)
 
 
+class GPRegression_EIV(GPModel):
+    r"""
+    Gaussian Process Regression model.
+
+    The core of a Gaussian Process is a covariance function :math:`k` which governs
+    the similarity between input points. Given :math:`k`, we can establish a
+    distribution over functions :math:`f` by a multivarite normal distribution
+
+    .. math:: p(f(X)) = \mathcal{N}(0, k(X, X)),
+
+    where :math:`X` is any set of input points and :math:`k(X, X)` is a covariance
+    matrix whose entries are outputs :math:`k(x, z)` of :math:`k` over input pairs
+    :math:`(x, z)`. This distribution is usually denoted by
+
+    .. math:: f \sim \mathcal{GP}(0, k).
+
+    .. note:: Generally, beside a covariance matrix :math:`k`, a Gaussian Process can
+        also be specified by a mean function :math:`m` (which is a zero-value function
+        by default). In that case, its distribution will be
+
+        .. math:: p(f(X)) = \mathcal{N}(m(X), k(X, X)).
+
+    Given inputs :math:`X` and their noisy observations :math:`y`, the Gaussian Process
+    Regression model takes the form
+
+    .. math::
+        f &\sim \mathcal{GP}(0, k(X, X)),\\
+        y & \sim f + \epsilon,
+
+    where :math:`\epsilon` is Gaussian noise.
+
+    .. note:: This model has :math:`\mathcal{O}(N^3)` complexity for training,
+        :math:`\mathcal{O}(N^3)` complexity for testing. Here, :math:`N` is the number
+        of train inputs.
+
+    Reference:
+
+    [1] `Gaussian Processes for Machine Learning`,
+    Carl E. Rasmussen, Christopher K. I. Williams
+
+    :param torch.Tensor X: A input data for training. Its first dimension is the number
+        of data points.
+    :param torch.Tensor y: An output data for training. Its last dimension is the
+        number of data points.
+    :param ~pyro.contrib.gp.kernels.kernel.Kernel kernel: A Pyro kernel object, which
+        is the covariance function :math:`k`.
+    :param torch.Tensor noise: Variance of Gaussian noise of this model.
+    :param callable mean_function: An optional mean function :math:`m` of this Gaussian
+        process. By default, we use zero mean.
+    :param float jitter: A small positive term which is added into the diagonal part of
+        a covariance matrix to help stablize its Cholesky decomposition.
+    """
+
+    def __init__(self, X, y, xerr,kernel, noise=None, mean_function=None, jitter=1e-6):
+        assert isinstance(
+            X, torch.Tensor
+        ), "X needs to be a torch Tensor instead of a {}".format(type(X))
+        if y is not None:
+            assert isinstance(
+                y, torch.Tensor
+            ), "y needs to be a torch Tensor instead of a {}".format(type(y))
+        
+        super().__init__(X, y,kernel, mean_function, jitter)
+
+        self.noise = noise
+        self.xerr = xerr
+    @pyro_method
+    def model(self):
+        self.set_mode("model")
+        N = self.X.size(0)
+        x_noise = pyro.sample('obs',dist.Normal(torch.zeros(N),self.xerr**0.5).to_event(1))
+        X_noisy = (self.X+x_noise)
+        Kff = self.kernel(X_noisy)
+        Kff.view(-1)[:: N + 1] += self.jitter + self.noise  # add noise to diagonal
+        Lff = torch.linalg.cholesky(Kff)
+        zero_loc = X_noisy.new_zeros(X_noisy.size(0))
+        f_loc = zero_loc + self.mean_function(X_noisy)
+        if self.y is None:
+            f_var = Lff.pow(2).sum(dim=-1)
+            return f_loc, f_var
+        else:
+
+            return pyro.sample(
+                self._pyro_get_fullname("y"),
+                dist.MultivariateNormal(f_loc, scale_tril=Lff)
+                .expand_by(self.y.shape[:-1])
+                .to_event(self.y.dim() - 1),
+                obs=self.y,
+            )
+
+
+    @pyro_method
+    def guide(self):
+        self.set_mode("guide")
+        self._load_pyro_samples()
+
+    def forward(self, Xnew, full_cov=False, noiseless=True):
+        r"""
+        Computes the mean and covariance matrix (or variance) of Gaussian Process
+        posterior on a test input data :math:`X_{new}`:
+
+        .. math:: p(f^* \mid X_{new}, X, y, k, \epsilon) = \mathcal{N}(loc, cov).
+
+        .. note:: The noise parameter ``noise`` (:math:`\epsilon`) together with
+            kernel's parameters have been learned from a training procedure (MCMC or
+            SVI).
+
+        :param torch.Tensor Xnew: A input data for testing. Note that
+            ``Xnew.shape[1:]`` must be the same as ``self.X.shape[1:]``.
+        :param bool full_cov: A flag to decide if we want to predict full covariance
+            matrix or just variance.
+        :param bool noiseless: A flag to decide if we want to include noise in the
+            prediction output or not.
+        :returns: loc and covariance matrix (or variance) of :math:`p(f^*(X_{new}))`
+        :rtype: tuple(torch.Tensor, torch.Tensor)
+        """
+        self._check_Xnew_shape(Xnew)
+        self.set_mode("guide")
+
+        N = self.X.size(0)
+        Kff = self.kernel(self.X).contiguous()
+        Kff.view(-1)[:: N + 1] += self.jitter + self.noise  # add noise to the diagonal
+        Lff = torch.linalg.cholesky(Kff)
+
+        y_residual = self.y - self.mean_function(self.X)
+        loc, cov = conditional(
+            Xnew,
+            self.X,
+            self.kernel,
+            y_residual,
+            None,
+            Lff,
+            full_cov,
+            jitter=self.jitter,
+        )
+
+        if full_cov and not noiseless:
+            M = Xnew.size(0)
+            cov = cov.contiguous()
+            cov.view(-1, M * M)[:, :: M + 1] += self.noise  # add noise to the diagonal
+        if not full_cov and not noiseless:
+            cov = cov + self.noise.abs()
+
+        return loc + self.mean_function(Xnew), cov
+
+
 def cal_rate_var(test_X,cov_matrix,mean_rsl,difftimestep=200):
     '''A function to caluclate standard deviation of sea-levle change rate (i.e., first derivative of 
     GP).
